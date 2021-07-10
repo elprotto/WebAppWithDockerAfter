a32c4d15f18084af9db13eb17ef31126ee12f722
Alexander Protto <elprotto@gmail.com>
2021-07-10 10:27:53 -0500

    Versioning implemented Added contracts and routes

---
+++ Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationWithDocker.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string root = "api";
+        public const string version = "v1";
+        public const string _base = root +"/" + version;
+        public static class Posts
+        {
+            public const string GetAll = _base + "/posts";
+        }
+    }
+}
---
+++ Controllers/V1/Posts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WebApplicationWithDocker.Contracts.V1;
+using WebApplicationWithDocker.Domain;
+
+namespace WebApplicationWithDocker.Controllers.V1
+{
+    public class Posts : Controller
+    {
+        private List<Post> posts;
+        public Posts()
+        {
+            this.posts = new List<Post>();
+            for (int i = 0; i < 100; i++)
+            {
+                this.posts.Add(new Post { Id = Guid.NewGuid().ToString()});
+            }
+        }
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(this.posts);
+        }
+    }
+}
