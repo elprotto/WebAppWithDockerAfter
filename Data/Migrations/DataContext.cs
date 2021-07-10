989bb0ac84cef7a7220baca125ae7f2bd897bac0
Alexander Protto <elprotto@gmail.com>
2021-07-10 07:29:12 -0500

    Added application db context

---
+++ Data/Migrations/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationWithDocker.Data.Migrations
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
